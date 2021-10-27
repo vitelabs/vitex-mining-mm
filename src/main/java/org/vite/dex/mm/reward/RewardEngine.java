21eda7b831ab4577a066870ca0d8928d209ee496
viteshan <wangjie@vite.org>
2021-10-27 20:47:59 +0800

    fix: space 4

--- src/main/java/org/vite/dex/mm/reward/RewardEngine.java
+++ src/main/java/org/vite/dex/mm/reward/RewardEngine.java
@@ -24,111 +24,113 @@ import java.util.List;
 @Slf4j
 @Component
 public class RewardEngine {
-        @Autowired
-        private ViteCli viteCli;
+	@Autowired
+	private ViteCli viteCli;
 
-        @Autowired
-        private MiningConfiguration miningConfig;
+	@Autowired
+	private MiningConfiguration miningConfig;
 
-        @Autowired
-        SettleService settleService;
+	@Autowired
+	private SettleService settleService;
 
-        /**
-         * run at 13:00 O`clock everyday and calc mining reward of each address during
-         * last cycle
-         * 
-         * @param prevTime yesterday 12:00 p.m.
-         * @param endTime  today 12:00 p.m.
-         * @throws Exception
-         */
-        @Scheduled(cron = "0 0 13 * * ?", zone = "Asia/Singapore")
-        public void runDaily() throws Exception {
-                int cycleKey = viteCli.getCurrentCycleKey();
-                log.info("the runDaily scheduler has been started! The cycleKey is: {}", cycleKey);
-                long endTime = CommonUtils.getTimestampByCyclekey(cycleKey);
-                long snapshotTime = endTime + 1800;
-                long prevTime = endTime - 86400;
+	/**
+	 * run at 13:00 O`clock everyday and calc mining reward of each address during
+	 * last cycle
+	 *
+	 * @param prevTime yesterday 12:00 p.m.
+	 * @param endTime  today 12:00 p.m.
+	 * @throws Exception
+	 */
+	@Scheduled(cron = "0 0 13 * * ?", zone = "Asia/Singapore")
+	public void runDaily() throws Exception {
+		int cycleKey = viteCli.getCurrentCycleKey();
+		log.info("the runDaily scheduler has been started! The cycleKey is: {}", cycleKey);
+		long endTime = CommonUtils.getTimestampByCyclekey(cycleKey);
+		long snapshotTime = endTime + 1800;
+		long prevTime = endTime - 86400;
 
-                Traveller traveller = new Traveller();
-                Tokens tokens = viteCli.getAllTokenInfos();
-                List<TradePair> tradePairs = CommonUtils
-                                .getMarketMiningTradePairs(miningConfig.getTradePairSettingUrl());
+		Traveller traveller = new Traveller();
+		Tokens tokens = viteCli.getAllTokenInfos();
+		List<TradePair> tradePairs = CommonUtils
+				.getMarketMiningTradePairs(miningConfig.getTradePairSettingUrl());
 
-                // 1.travel to snapshot time
-                OrderBooks snapshotOrderBooks = traveller.travelInTime(snapshotTime, tokens, viteCli, tradePairs);
-                log.info("travel to checkpoint successfully, the snapshotOrderBooks`s currentHeight is {}",
-                                snapshotOrderBooks.getCurrentHeight());
+		// 1.travel to snapshot time
+		OrderBooks snapshotOrderBooks = traveller.travelInTime(snapshotTime, tokens, viteCli, tradePairs);
+		log.info("travel to checkpoint successfully, the snapshotOrderBooks`s currentHeight is {}",
+				snapshotOrderBooks.getCurrentHeight());
 
-                // 2.recover orderbooks
-                TradeRecover tradeRecover = new TradeRecover();
-                TradeRecover.RecoverResult recoverResult = tradeRecover.recoverInTime(snapshotOrderBooks, prevTime,
-                                tokens, viteCli);
-                OrderBooks recoveredOrderBooks = recoverResult.getOrderBooks();
-                BlockEventStream stream = recoverResult.getStream();
-                stream.patchTimestampToOrderEvent(viteCli);
-                tradeRecover.fillAddressForOrdersGroupByTimeUnit(recoveredOrderBooks.getBooks(), viteCli);
-                log.info("recovered to cycle`s startTime successfully, the recoveredOrderBooks`s currentHeight is {},the eventStream startHeight {} and endHeight {}",
-                                recoveredOrderBooks.getCurrentHeight(), stream.getStartHeight(), stream.getEndHeight());
+		// 2.recover orderbooks
+		TradeRecover tradeRecover = new TradeRecover();
+		TradeRecover.RecoverResult recoverResult = tradeRecover.recoverInTime(snapshotOrderBooks, prevTime,
+				tokens, viteCli);
+		OrderBooks recoveredOrderBooks = recoverResult.getOrderBooks();
+		BlockEventStream stream = recoverResult.getStream();
+		stream.patchTimestampToOrderEvent(viteCli);
+		tradeRecover.fillAddressForOrdersGroupByTimeUnit(recoveredOrderBooks.getBooks(), viteCli);
+		log.info(
+				"recovered to cycle`s startTime successfully, the recoveredOrderBooks`s currentHeight is {},the eventStream startHeight {} and endHeight {}",
+				recoveredOrderBooks.getCurrentHeight(), stream.getStartHeight(), stream.getEndHeight());
 
-                // 3.market-mining
-                BigDecimal totalReleasedVxAmount = CommonUtils.getVxAmountByCycleKey(cycleKey - 1);
-                RewardKeeper rewardKeeper = new RewardKeeper(viteCli, miningConfig);
-                FinalResult finalRes = rewardKeeper.calcAddressMarketReward(recoveredOrderBooks, stream,
-                                totalReleasedVxAmount, prevTime, endTime);
-                log.info("succeed to calc each address`s market mining rewards, the result {}", finalRes);
+		// 3.market-mining
+		BigDecimal totalReleasedVxAmount = CommonUtils.getVxAmountByCycleKey(cycleKey - 1);
+		RewardKeeper rewardKeeper = new RewardKeeper(viteCli, miningConfig);
+		FinalResult finalRes = rewardKeeper.calcAddressMarketReward(recoveredOrderBooks, stream,
+				totalReleasedVxAmount, prevTime, endTime);
+		log.info("succeed to calc each address`s market mining rewards, the result {}", finalRes);
 
-                // 4. save reward results to DB
-                settleService.saveMiningRewards(finalRes, totalReleasedVxAmount, cycleKey - 1);
-        }
+		// 4. save reward results to DB
+		settleService.saveMiningRewards(finalRes, totalReleasedVxAmount, cycleKey - 1);
+	}
 
-        /**
-         * run every half hour and estimate mining reward of each address during this
-         * cycle (not the last cycle but this current ones)
-         * 
-         * @param estimateNodeTime
-         * @throws Exception
-         */
-        @Scheduled(cron = "0 30 * * * ?", zone = "Asia/Singapore")
-        public void runHalfHour() throws Exception {
-                int cycleKey = viteCli.getCurrentCycleKey();
-                log.info("the runHalfHour scheduler has been started! The cycleKey is: {}", cycleKey);
-                long startTime = CommonUtils.getTimestampByCyclekey(cycleKey);
-                long estimateTime = System.currentTimeMillis() / 1000;
-                if (estimateTime < startTime) {
-                        startTime = startTime - 86400;
-                }
-                long snapshotTime = estimateTime - 10 * 60;
+	/**
+	 * run every half hour and estimate mining reward of each address during this
+	 * cycle (not the last cycle but this current ones)
+	 *
+	 * @param estimateNodeTime
+	 * @throws Exception
+	 */
+	@Scheduled(cron = "0 30 * * * ?", zone = "Asia/Singapore")
+	public void runHalfHour() throws Exception {
+		int cycleKey = viteCli.getCurrentCycleKey();
+		log.info("the runHalfHour scheduler has been started! The cycleKey is: {}", cycleKey);
+		long startTime = CommonUtils.getTimestampByCyclekey(cycleKey);
+		long estimateTime = System.currentTimeMillis() / 1000;
+		if (estimateTime < startTime) {
+			startTime = startTime - 86400;
+		}
+		long snapshotTime = estimateTime - 10 * 60;
 
-                Traveller traveller = new Traveller();
-                Tokens tokens = viteCli.getAllTokenInfos();
-                List<TradePair> tradePairs = CommonUtils
-                                .getMarketMiningTradePairs(miningConfig.getTradePairSettingUrl());
+		Traveller traveller = new Traveller();
+		Tokens tokens = viteCli.getAllTokenInfos();
+		List<TradePair> tradePairs = CommonUtils
+				.getMarketMiningTradePairs(miningConfig.getTradePairSettingUrl());
 
-                // 1.travel to 10 minutes age
-                OrderBooks snapshotOrderBooks = traveller.travelInTime(snapshotTime, tokens, viteCli, tradePairs);
-                log.info("travel to checkpoint successfully, the snapshotOrderBooks`s currentHeight is {}",
-                                snapshotOrderBooks.getCurrentHeight());
+		// 1.travel to 10 minutes age
+		OrderBooks snapshotOrderBooks = traveller.travelInTime(snapshotTime, tokens, viteCli, tradePairs);
+		log.info("travel to checkpoint successfully, the snapshotOrderBooks`s currentHeight is {}",
+				snapshotOrderBooks.getCurrentHeight());
 
-                // 2.recover orderbooks
-                TradeRecover tradeRecover = new TradeRecover();
-                TradeRecover.RecoverResult recoverResult = tradeRecover.recoverInTime(snapshotOrderBooks, startTime,
-                                tokens, viteCli);
-                OrderBooks recoveredOrderBooks = recoverResult.getOrderBooks();
-                BlockEventStream stream = recoverResult.getStream();
-                stream.patchTimestampToOrderEvent(viteCli);
-                tradeRecover.fillAddressForOrdersGroupByTimeUnit(recoveredOrderBooks.getBooks(), viteCli);
-                log.info("recovered to cycle`s startTime successfully, the recoveredOrderBooks`s currentHeight is {},the eventStream startHeight {} and endHeight {}",
-                                recoveredOrderBooks.getCurrentHeight(), stream.getStartHeight(), stream.getEndHeight());
+		// 2.recover orderbooks
+		TradeRecover tradeRecover = new TradeRecover();
+		TradeRecover.RecoverResult recoverResult = tradeRecover.recoverInTime(snapshotOrderBooks, startTime,
+				tokens, viteCli);
+		OrderBooks recoveredOrderBooks = recoverResult.getOrderBooks();
+		BlockEventStream stream = recoverResult.getStream();
+		stream.patchTimestampToOrderEvent(viteCli);
+		tradeRecover.fillAddressForOrdersGroupByTimeUnit(recoveredOrderBooks.getBooks(), viteCli);
+		log.info(
+				"recovered to cycle`s startTime successfully, the recoveredOrderBooks`s currentHeight is {},the eventStream startHeight {} and endHeight {}",
+				recoveredOrderBooks.getCurrentHeight(), stream.getStartHeight(), stream.getEndHeight());
 
-                // 3.market-mining
-                RewardKeeper rewardKeeper = new RewardKeeper(viteCli, miningConfig);
-                
-                BigDecimal totalReleasedVxAmount = CommonUtils.getVxAmountByCycleKey(cycleKey);
-                FinalResult finalRes = rewardKeeper.calcAddressMarketReward(recoveredOrderBooks, stream,
-                                totalReleasedVxAmount, startTime, estimateTime);
-                log.info("succeed to calc each address`s market mining rewards, the result {}", finalRes);
+		// 3.market-mining
+		RewardKeeper rewardKeeper = new RewardKeeper(viteCli, miningConfig);
 
-                // 4.save estimated result to DB
-                settleService.saveOrderMiningEstimateRes(finalRes.getOrderMiningFinalRes(), cycleKey);
-        }
+		BigDecimal totalReleasedVxAmount = CommonUtils.getVxAmountByCycleKey(cycleKey);
+		FinalResult finalRes = rewardKeeper.calcAddressMarketReward(recoveredOrderBooks, stream,
+				totalReleasedVxAmount, startTime, estimateTime);
+		log.info("succeed to calc each address`s market mining rewards, the result {}", finalRes);
+
+		// 4.save estimated result to DB
+		settleService.saveOrderMiningEstimateRes(finalRes.getOrderMiningFinalRes(), cycleKey);
+	}
 }
